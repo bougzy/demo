7678ae48090ad99b357f472054957be19ec4386c
forkhivetech <forkhivetech@gmail.com>
2024-12-30 06:42:59 +0100

    ups

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
